3ed20684e2fd96832eef5a21cdea3523bc97e786
solunara <soluna@yeah.net>
2025-04-16 13:59:26 +0800

    完成用户列表基础表格布局

--- src/views/user/List.vue
+++ src/views/user/List.vue
@@ -1,13 +1,122 @@
 <template>
-    <div>
-        <h6>list user</h6>
-    </div>
+    <el-card>
+        <template #header>
+            <div class="card-header">
+                <span>用户列表</span>
+                <el-button class="button">添加</el-button>
+            </div>
+        </template>
+        <el-table
+            :data="tableData"
+            style="width: 100%; height: 100%"
+        >
+            <el-table-column sortable fixed property="date" label="Date" width="120" />
+            <el-table-column property="name" label="Name" width="180" />
+            <el-table-column prop="state" label="State" width="120" />
+            <el-table-column prop="city" label="City" width="120" />
+            <el-table-column prop="address" label="Address" width="600" />
+            <el-table-column prop="zip" label="Zip" width="120" />
+            <el-table-column fixed="right" label="Operations" min-width="120">
+            <template #default>
+                <el-button link type="primary" size="small" @click="handleClick">
+                    Detail
+                </el-button>
+                <el-button link type="primary" size="small">
+                    Edit
+                </el-button>
+            </template>
+            </el-table-column>
+        </el-table>
+        <!-- <template #footer>Footer content</template> -->
+    </el-card>
 </template>
 
 <script setup>
-
+const tableData = [
+{
+    date: '2016-05-03',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Home',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Office',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Home',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Office',
+  },
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Home',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Office',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Home',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Office',
+  },
+]
 </script>
 
 <style lang="scss" scoped>
-
+.card-header {
+    display: flex;
+    justify-content: space-between;
+}
+.text {
+    font-size: 14px;
+}
+.item {
+    margin-bottom: 18px;
+}
 </style>
